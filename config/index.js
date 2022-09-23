958c499e7ae9c606326ba168e2bf586fc99e3d74
chaolinli <114225600+chaolinli@users.noreply.github.com>
2022-09-23 18:24:10 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -70,7 +70,7 @@ export const USER_CONFIG = {
       // 所在城市
       city: "长沙",
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '12-27',
+      horoscopeDate: '09-09',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
@@ -134,7 +134,7 @@ export const USER_CONFIG = {
    */
   
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "",
+  CALLBACK_TEMPLATE_ID: "fR2ZwPnVnqNUYMQNYPgBwnGIpJ1D77KVGuFBlsg_OJg",
   
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
@@ -142,7 +142,7 @@ export const USER_CONFIG = {
       // 一般都填自己
       name: "自己",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "oantg52XpJ0cXCWaEoPFNDC6n314",
     },
     // 你可以不断按格式往下增加
     // ...
@@ -155,9 +155,9 @@ export const USER_CONFIG = {
   /** 天气相关 */
   
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: "广东",
+  PROVINCE: "湖南",
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: "惠州",
+  CITY: "长沙",
   
   /** 重要节日相关 */
   
