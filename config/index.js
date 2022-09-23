b93f5fafdf84ee014632b1df5f47132f568f021c
chaolinli <114225600+chaolinli@users.noreply.github.com>
2022-09-23 18:35:22 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ export const USER_CONFIG = {
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "87adbea62e7d81cee5802da434df32bc",
+  APP_SECRET: "12ba1a8127edb7227f41f8ecfe68a42c",
   
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
